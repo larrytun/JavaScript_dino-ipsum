2503243a1ef096cd9ecfe92f3c37b38a255b6eae
Blake Scala and Larry Tunseth <larry@>
2016-10-12 12:56:24 -0700

    add div for response output

---
+++ tmp/allConcat.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var words = $("#words").val();
+    var paragraphs = $("#paragraphs").val();
+    $.get('http://dinoipsum.herokuapp.com/api/?format=html&words=' + words + '&paragraphs=' + paragraphs).then(function(response) {
+      $("#output").text(response);
+    });
+  });
+});
